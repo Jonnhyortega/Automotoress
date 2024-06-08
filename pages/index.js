946ff39a924c449e849e8b8e84a15edd22cdd0ff
Jonathan Ortega <132107501+Jonnhyortega@users.noreply.github.com>
2024-06-07 21:01:11 -0300

    pages

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>¡Bienvenido a mi aplicación!</h1>
+      <p>Esta es la página principal de mi aplicación.</p>
+    </div>
+  );
+};
+
+export default HomePage;
